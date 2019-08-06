36cf1a8d463461d0a722961b8b1f4c69abc5a285
Jonathan Lalande <lalandej.gg@gmail.com>
2019-08-06 13:12:36 -0400

    add difficulty inside block

--- Purecoin/Block.hs
+++ Purecoin/Block.hs
@@ -13,19 +13,22 @@ import qualified Crypto.Hash.SHA256 as SHA256
 data Block = Block
   { bPreviousHash :: String
   , bTimestamp :: Integer
-  , bNounce :: Integer }
+  , bNounce :: Integer
+  , bDifficulty :: Integer }
 
 genesis :: Block
 genesis = Block
   { bPreviousHash="0000000000000000000000000000000000000000000000000000000000000000"
   , bTimestamp=1565062105
-  , bNounce=0 }
+  , bNounce=0
+  , bDifficulty=4}
 
 createBlock :: Integer -> String -> Block
 createBlock t prev = Block
   { bPreviousHash=prev
   , bTimestamp=t
-  , bNounce=0 }
+  , bNounce=0
+  , bDifficulty=4}
 
 blockHash :: Block -> String
 blockHash b = show . B16.encode $ hash
@@ -38,5 +41,6 @@ instance Show Block where
     , "previous hash: \t" ++ bPreviousHash b
     , "timestamp: \t" ++ show (bTimestamp b)
     , "nounce: \t" ++ show (bNounce b)
+    , "difficulty: \t" ++ show (bDifficulty b)
     , "=========================================="
     ]
